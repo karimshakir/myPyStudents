cf0f556b068904071c9c6c477ea489601d1f86f0
“karimshakir” <karimshakir99@gmail.com>
2020-11-23 10:52:37 -0600

    completes loops

--- loops.py
+++ loops.py
@@ -6,9 +6,9 @@ while True:
 
     user_input  =  input('Would you like to play? (Y/n) ') #will default to Y  
 
-    if user_input == 'n'
+    if user_input == 'n':
         break
-        
+
     user_number = int(input('Guess our number: '))
     if user_number == number:
         print ('You guessed correctly!!')
@@ -17,4 +17,16 @@ while True:
     else:
         print('Sorry, it\'s wrong!')
     
-    
+friends = ['rolf', 'jen', 'bob', 'anny']
+
+for friend in friends:
+    print(f'{friend} is my friend. ')
+
+for friend in range(3):
+    print(f'{friend} is my friend. ')
+
+numbers  = [1, 2, 3, 4, 5, 6]
+total  = sum(numbers)
+amount = len(numbers)
+print(total)
+print(amount)
