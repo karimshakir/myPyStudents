c4eb198b2c95a011628aace6302d8e25860699cc
“karimshakir” <karimshakir99@gmail.com>
2020-11-24 07:37:15 -0600

    this completes dictionaries

--- dictionaries.py
+++ dictionaries.py
@@ -1,2 +1,13 @@
 friend_ages = {'rolf': 24,
                 'adam': 30 }
+friend_ages['cathy'] = 34
+print(friend_ages)
+
+
+for k, v in friend_ages.items():
+    print(v)
+
+    #Destructering variables
+    t = 5,11
+    x,y = t
+    print(x, y)
